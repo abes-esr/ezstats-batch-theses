ee76a375a45f853ef5525747af47b90cc97773e0
a <a>
2024-06-25 11:09:18 +0200

    test v3

---
+++ docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+{ echo ""; \
+  echo "# EZP Bulk"; \
+  echo "* * * * * /home/node/launch-ezp.sh 1>/proc/1/fd/1 2>/proc/1/fd/2"; \
+	} | crontab -
+
+# start cron
+echo "Starting EZP bulk cron..."
+#service cron start
+cron -f
