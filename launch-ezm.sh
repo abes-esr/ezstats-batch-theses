8a0cb3653d60312dba9315391fd0221a46772889
a <a>
2024-08-22 11:43:03 +0200

    ezmesure avec param -r recursive

--- launch-ezm.sh
+++ launch-ezm.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-source /home/node/.env
-
 # URL de ezmesure (à adapter selon votre configuration)
 RESULTS_DIR="/home/node/logtheses/data/thesesfr/results"
 
 cd /home/node
 
-ezmesure -t "$EZMESURE_TOKEN"  indices insert z-abes-ezpaarse $RESULTS_DIR/*.csv
+ezmesure -t "$EZMESURE_TOKEN"  indices insert  z-abes-ezpaarse -r $RESULTS_DIR
