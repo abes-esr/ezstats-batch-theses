66bc12a5202cdb50b881060ed090d3caec39f16d
Aurélien Charot <charot@abes.fr>
2024-08-29 11:53:11 +0200

    Ajout de -H "ezPAARSE-Filter-Redirects: false"

--- launch-ezm.sh
+++ launch-ezm.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+#Batch qui lance ezmesure sur le repertoire logtheses/data/thesesfr/results/ avec le token EZMesure recu en parametre $1
+
 PATH=/opt/ezpaarse/bin:/opt/ezpaarse/node_modules/.bin:/usr/local/bin:/usr/bin:/bin:/sbin:/usr/sbin:/usr/local/sbin:$PATH
 NODE_ENV=production
 
 RESULTS_DIR="/home/node/logtheses/data/thesesfr/results"
 
-cd /home/node
-
-ezmesure -t "$1"  indices insert  z-abes-ezpaarse -r $RESULTS_DIR
+if [[ $(ps -edf | grep -c "launch-ezm.sh") = 4 ]];then
+  cd /home/node
+  ezmesure -t "$1"  indices insert  z-abes-ezpaarse -r $RESULTS_DIR
+  chmod -R 777 $RESULTS_DIR
+else
+  dt=$(date '+%d/%m/%Y %H:%M:%S')
+  echo "$dt launch-ezm.sh s'execute deja"
+fi
--- launch-ezp.sh
+++ launch-ezp.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+#Batch qui lance ezpaarse sur le repertoire logtheses/data/thesesfr/logs/ et ecrit les resultats dans logtheses/data/thesesfr/results/
+
 PATH=/opt/ezpaarse/bin:/opt/ezpaarse/node_modules/.bin:/usr/local/bin:/usr/bin:/bin:/sbin:/usr/sbin:/usr/local/sbin:$PATH
 NODE_ENV=production
 
-cd /home/node
-ezp bulk -r -v logtheses/data/thesesfr/logs/ logtheses/data/thesesfr/results/ \
-                -h ezstats-ezpaarse:59599 \
-                -H "Force-Parser: thesesfr" \
-                -H "filter-platforms: thesesfr" \
-                -H "ezPAARSE-Filter-Redirects: false" \
-                -H "ezPAARSE-Middlewares: thesesfr,thesesfr-personne" \
-                -H "Output-Fields: +nnt, +numSujet, +etabSoutenanceN, +etabSoutenancePpn, +dateSoutenance, +dateInscription, +statut, +accessible, +source, +discipline, +domaine, +langue, +ecoleDoctoraleN, +ecoleDoctoralePpn, +partenaireRechercheN, +partenaireRecherchePpn, +cotutelleN, +cotutellePpn, +auteurN, +auteurPpn, +directeurN, +directeurPpn, +presidentN, +presidentPpn, +rapporteursN, +rapporteursPpn, +membresN, +membresPpn, +personneN, +personnePpn, +organismeN, +organismePpn" \
-                -H "Log-Format-apache: %h %l %u %t \"%r\" %>s %b \"%{Referer}<.*>\" \"%{User-Agent}<.*>\" \"%{Shib-Identity-Provider}<.*>\" \"%{eppn}<.*>\" \"%{primary-affiliation}<.*>\" \"%{supannEtablissement}<.*>\""
-
+if [[ $(ps -edf | grep -c "launch-ezp.sh") = 4 ]];then
+  cd /home/node
+  ezp bulk -r -v logtheses/data/thesesfr/logs/ logtheses/data/thesesfr/results/ \
+                  -h ezstats-ezpaarse:59599 \
+                  -H "Force-Parser: thesesfr" \
+                  -H "filter-platforms: thesesfr" \
+                  -H "ezPAARSE-Filter-Redirects: false" \
+                  -H "ezPAARSE-Middlewares: thesesfr,thesesfr-personne" \
+                  -H "Output-Fields: +nnt, +numSujet, +etabSoutenanceN, +etabSoutenancePpn, +dateSoutenance, +dateInscription, +statut, +accessible, +source, +discipline, +domaine, +langue, +ecoleDoctoraleN, +ecoleDoctoralePpn, +partenaireRechercheN, +partenaireRecherchePpn, +cotutelleN, +cotutellePpn, +auteurN, +auteurPpn, +directeurN, +directeurPpn, +presidentN, +presidentPpn, +rapporteursN, +rapporteursPpn, +membresN, +membresPpn, +personneN, +personnePpn, +organismeN, +organismePpn" \
+                  -H "Log-Format-apache: %h %l %u %t \"%r\" %>s %b \"%{Referer}<.*>\" \"%{User-Agent}<.*>\" \"%{Shib-Identity-Provider}<.*>\" \"%{eppn}<.*>\" \"%{primary-affiliation}<.*>\" \"%{supannEtablissement}<.*>\""
+  chmod -R 777 logtheses/data/thesesfr/results/
+else
+  dt=$(date '+%d/%m/%Y %H:%M:%S')
+  echo "$dt launch-ezp.sh s'execute deja"
+fi
